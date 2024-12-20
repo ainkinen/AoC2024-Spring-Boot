367e4791d23567e8cb39a94bae6887b07c13a114
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2024-12-20 22:14:34 +0200

    Init

---
+++ src/main/java/com/advent/aoc2024/AoC2024JavaApplication.java
@@ -0,0 +1,13 @@
+package com.advent.aoc2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoC2024JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoC2024JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advent/aoc2024/AoC2024JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advent.aoc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoC2024JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
