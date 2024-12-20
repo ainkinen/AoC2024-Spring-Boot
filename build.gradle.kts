4d4bcdb6f29b79bcb969c7899739ee621050bba8
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2024-12-20 22:53:17 +0200

    Force bump internal dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // https://github.com/spring-projects/spring-boot/issues/43568
+    // Force bumping internal Spring Boot dependencies
+    implementation("ch.qos.logback:logback-classic:1.5.14+")
+    implementation("ch.qos.logback:logback-core:1.5.14+")
 }
 
 tasks.withType<Test> {
