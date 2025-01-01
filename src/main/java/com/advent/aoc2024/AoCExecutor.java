5ced026cf23026b275cdb06a8fabfc3287607d1d
Antti-Jussi Inkinen <aj.inkinen@gmail.com>
2025-01-02 00:29:29 +0200

    Allow running individual days

--- src/main/java/com/advent/aoc2024/AoCExecutor.java
+++ src/main/java/com/advent/aoc2024/AoCExecutor.java
@@ -7,6 +7,7 @@ import com.advent.aoc2024.interfaces.OnePartOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +19,18 @@ import java.util.function.Function;
 public class AoCExecutor {
     private static final Logger logger = LogManager.getLogger();
 
-    private final InputReader inputReader;
     private final ApplicationContext applicationContext;
+    private final ApplicationArguments applicationArguments;
+    private final InputReader inputReader;
 
     @Autowired
-    public AoCExecutor(InputReader reader, ApplicationContext applicationContext) {
-        this.inputReader = reader;
+    public AoCExecutor(
+            ApplicationContext applicationContext,
+            ApplicationArguments applicationArguments,
+            InputReader reader) {
         this.applicationContext = applicationContext;
+        this.applicationArguments = applicationArguments;
+        this.inputReader = reader;
     }
 
     private static void execPart(
@@ -43,21 +49,42 @@ public class AoCExecutor {
     }
 
     public void run() {
+        if (applicationArguments.containsOption("day")) {
+            runOne(applicationArguments.getOptionValues("day").getFirst());
+        } else {
+            runAll();
+        }
+    }
+
+    private void runDay(Day day) {
+        String name = day.getClass().getSimpleName();
+
+        String input = this.inputReader.read(name);
+
+        if (day instanceof OnePartOnly onePartOnly) {
+            execPart(name, "Part 1", onePartOnly::part1, input);
+        }
+
+        if (day instanceof BothParts completeDay) {
+            execPart(name, "Part 2", completeDay::part2, input);
+        }
+    }
+
+    private String getDay(String input) {
+        return String.format("day%02d", Integer.parseInt(input));
+    }
+
+    private void runOne(String dayInput) {
+        Day day = applicationContext.getBean(getDay(dayInput), Day.class);
+
+        runDay(day);
+    }
+
+    private void runAll() {
         List<Day> days = applicationContext.getBeansOfType(Day.class).values().stream().toList();
 
         for (Day day : days) {
-
-            String name = day.getClass().getSimpleName();
-
-            String input = this.inputReader.read(name);
-
-            if (day instanceof OnePartOnly onePartOnly) {
-                execPart(name, "Part 1", onePartOnly::part1, input);
-            }
-
-            if (day instanceof BothParts completeDay) {
-                execPart(name, "Part 2", completeDay::part2, input);
-            }
+            runDay(day);
         }
     }
 }
